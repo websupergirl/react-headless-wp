0887e370255f3ee2f39a2cb7b4744047593ef957
Nikole Gipps <nikole@thatsupergirl.com>
2019-09-16 21:18:24 -0700

    Updates

---
+++ precache-manifest.34d50cd2b4e5801da159079365a15783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e115f669c29ce04550ab559d5817b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e749265c3371908601c0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "95e8b257f960f2a7020f",
+    "url": "/static/js/2.63b4584b.chunk.js"
+  },
+  {
+    "revision": "e749265c3371908601c0",
+    "url": "/static/js/main.847eb954.chunk.js"
+  },
+  {
+    "revision": "fe3d51059898f4b01c5f",
+    "url": "/static/js/runtime~main.ad7ce8b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-headless-wp/precache-manifest.35f9a730517793e2f55906dae21d8a08.js"
+  "/precache-manifest.34d50cd2b4e5801da159079365a15783.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-headless-wp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
